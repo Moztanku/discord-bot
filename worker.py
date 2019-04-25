9de09e0b6049a03abe6d70fafee4e5a4875fadc2
Moztanku <47280591+Moztanku@users.noreply.github.com>
2019-04-25 20:31:42 +0200

    Update worker.py

--- worker.py
+++ worker.py
@@ -7,15 +7,15 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("Gotowy do pracy 10")
+    print("Gotowy do pracy 6")
 @client.event
 async def on_message(message):
     if 'nigger' in message.content.lower() or 'nigga' in message.content.lower():
-        if "569979207292157984" not in [role.id for role in message.author.roles]:
+        if not discord.utils.get(message.author.roles, name="N-Word Pass"):
             await message.delete()
             await message.channel.send( message.author.mention+" nie masz N-Word Passa, proszę nie używać słowa \"ni\*\*er\".")
     if 'spermiarz' in message.content.lower():
-        if "569979214858551298" not in [role.id for role in message.author.roles]:
+        if not discord.utils.get(message.author.roles, name="S-Word Pass"):
             await message.delete()
             await message.channel.send( message.author.mention+" nie masz S-Word Passa, proszę nie używać słowa \"sp\*\*\*\*\*rz\".")
 client.run("NTAzNTY0ODMzMzU1MjAyNTcz.Dq4YRA.9MLbFuNBjL6ZjnNHJXvWwPH9Ffw")
