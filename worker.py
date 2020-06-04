0d8b0848d5d13940e54bb85a42c1ba84d905a0d1
Moztanku <47280591+Moztanku@users.noreply.github.com>
2020-06-04 15:26:02 +0200

    Update worker.py

--- worker.py
+++ worker.py
@@ -7,15 +7,14 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("Gotowy do pracy 6")
+    print("Ready to work")
 @client.event
 async def on_message(message):
-    if 'nigger' in message.content.lower() or 'nigga' in message.content.lower():
-        if not discord.utils.get(message.author.roles, name="N-Word Pass"):
+    if '/*YOUR WORD*/' in message.content.lower() or '/*YOUR OTHER WORD*/' in message.content.lower():
+        if not discord.utils.get(message.author.roles, name="/*REQUIRED RANK NAME*/"):
             await message.delete()
-            await message.channel.send( message.author.mention+" nie masz N-Word Passa, proszę nie używać słowa \"ni\*\*er\".")
-    if 'spermiarz' in message.content.lower():
-        if not discord.utils.get(message.author.roles, name="S-Word Pass"):
+            await message.channel.send( message.author.mention+" cant say the \**\ word.")
+    if '/*YOUR WORD*/' in message.content.lower():
+        if not discord.utils.get(message.author.roles, name="/*REQUIRED RANK*/"):
             await message.delete()
-            await message.channel.send( message.author.mention+" nie masz S-Word Passa, proszę nie używać słowa \"sp\*\*\*\*\*rz\".")
-client.run("NTAzNTY0ODMzMzU1MjAyNTcz.Dq4YRA.9MLbFuNBjL6ZjnNHJXvWwPH9Ffw")
+            await message.channel.send( message.author.mention+" doesn't have a necessary rank to use \**\ word.")
